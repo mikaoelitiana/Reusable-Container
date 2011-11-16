1555af50ad9a59a3e733765cf04d45750acbcca5
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2011-11-16 13:41:08 +0300

    Meta dans l'ApplicationHelper

--- reusable_containers/app/helpers/reusable_containers/application_helper.rb
+++ reusable_containers/app/helpers/reusable_containers/application_helper.rb
@@ -1,4 +1,24 @@
 module ReusableContainers
   module ApplicationHelper
+    def dark_side?
+      Rails.logger.debug request.fullpath
+      request.fullpath =~ /\/admin/
+    end
+
+    def meta
+      @meta ||= Meta.instance
+      unless @meta.title?
+        @meta.title= "#{self.action_name} | #{self.controller_name}"
+      end
+      @meta
+    end
+
+    def current_entry
+      @entry ||= Entry.find(params[:current_entry_id], :include => :container)
+    end
+
+    def current_container
+      @current_container ||= current_entry.container
+    end
   end
 end
