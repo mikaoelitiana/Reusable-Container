6ddee7af1626d1a09c8b0d6145600deed967f037
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2011-11-22 08:17:42 +0300

    Suppression de 'helper_method'

--- reusable_containers/app/controllers/reusable_containers/application_controller.rb
+++ reusable_containers/app/controllers/reusable_containers/application_controller.rb
@@ -1,9 +1,7 @@
 module ReusableContainers
   class ApplicationController < ActionController::Base
     protect_from_forgery
-    helper_method :dark_side?, :meta, :current_entry, :current_container
-
+    #helper_method :dark_side?, :meta, :current_entry, :current_container, :public_root_path
     layout Proc.new { |ctrl| dark_side? ? "application" : "containers/layouts/#{current_container.layout}" }
-
   end
 end
